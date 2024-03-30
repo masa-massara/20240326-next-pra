461943ed752261fd1556629003eaba01a297ad83
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-03-30 15:23:22 +0900

    import "./globals.css";が抜けていたので修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
